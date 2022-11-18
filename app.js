2b6690a6b2efc609825b3d0f061ecf51150c276f
trianhdev <117836328+trianhdev@users.noreply.github.com>
2022-11-18 11:09:22 +0700

    v5

--- app.js
+++ app.js
@@ -38,6 +38,7 @@ const userSchema = mongoose.Schema({
     require: true,
     default: false,
   },
+  balance:{type:Number, default:0}
 });
 const User = mongoose.model("user", userSchema);
 
@@ -94,296 +95,7 @@ const matchSchema = mongoose.Schema({
   rate1: Number,
   rate2: Number,
 });
-const matchArr = [
-  {
-    matchday: "2022-11-20",
-    matchtime: "19:00",
-    country1: "Qatar",
-    country2: "Ecuador",
-  },
-  {
-    matchday: "2022-11-21",
-    matchtime: "19:00",
-    country1: "Senegal",
-    country2: "Netherlands",
-  },
-  {
-    matchday: "2022-11-21",
-    matchtime: "16:00",
-    country1: "England",
-    country2: "Iran",
-  },
-  {
-    matchday: "2022-11-21",
-    matchtime: "22:00",
-    country1: "United States",
-    country2: "Wales",
-  },
-  {
-    matchday: "2022-11-22",
-    matchtime: "13:00",
-    country1: "Argentina",
-    country2: "Saudi Arabia",
-  },
-  {
-    matchday: "2022-11-22",
-    matchtime: "16:00",
-    country1: "Denmark",
-    country2: "Tunisia",
-  },
-  {
-    matchday: "2022-11-22",
-    matchtime: "19:00",
-    country1: "Mexico",
-    country2: "Poland",
-  },
-  {
-    matchday: "2022-11-22",
-    matchtime: "22:00",
-    country1: "France",
-    country2: "Australia",
-  },
-  {
-    matchday: "2022-11-23",
-    matchtime: "13:00",
-    country1: "Morocco",
-    country2: "Croatia",
-  },
-  {
-    matchday: "2022-11-23",
-    matchtime: "16:00",
-    country1: "Germany",
-    country2: "Japan",
-  },
-  {
-    matchday: "2022-11-23",
-    matchtime: "19:00",
-    country1: "Spain",
-    country2: "Costa Rica",
-  },
-  {
-    matchday: "2022-11-23",
-    matchtime: "22:00",
-    country1: "Belgium",
-    country2: "Canada",
-  },
-  {
-    matchday: "2022-11-24",
-    matchtime: "13:00",
-    country1: "Switzerland",
-    country2: "Cameroon",
-  },
-  {
-    matchday: "2022-11-24",
-    matchtime: "16:00",
-    country1: "Uruguay",
-    country2: "Korea Republic",
-  },
-  {
-    matchday: "2022-11-24",
-    matchtime: "19:00",
-    country1: "Portugal",
-    country2: "Ghana",
-  },
-  {
-    matchday: "2022-11-24",
-    matchtime: "22:00",
-    country1: "Brazil",
-    country2: "Serbia",
-  },
-  {
-    matchday: "2022-11-25",
-    matchtime: "13:00",
-    country1: "Wales",
-    country2: "Iran",
-  },
-  {
-    matchday: "2022-11-25",
-    matchtime: "16:00",
-    country1: "Qatar",
-    country2: "Senegal",
-  },
-  {
-    matchday: "2022-11-25",
-    matchtime: "19:00",
-    country1: "Netherlands",
-    country2: "Ecuador",
-  },
-  {
-    matchday: "2022-11-25",
-    matchtime: "22:00",
-    country1: "England",
-    country2: "United States",
-  },
-  {
-    matchday: "2022-11-26",
-    matchtime: "13:00",
-    country1: "Tunisia",
-    country2: "Australia",
-  },
-  {
-    matchday: "2022-11-26",
-    matchtime: "16:00",
-    country1: "Poland",
-    country2: "Saudi Arabia",
-  },
-  {
-    matchday: "2022-11-26",
-    matchtime: "19:00",
-    country1: "France",
-    country2: "Denmark",
-  },
-  {
-    matchday: "2022-11-26",
-    matchtime: "22:00",
-    country1: "Argentina",
-    country2: "Mexico",
-  },
-  {
-    matchday: "2022-11-27",
-    matchtime: "13:00",
-    country1: "Japan",
-    country2: "Costa Rica",
-  },
-  {
-    matchday: "2022-11-27",
-    matchtime: "16:00",
-    country1: "Belgium",
-    country2: "Morocco",
-  },
-  {
-    matchday: "2022-11-27",
-    matchtime: "19:00",
-    country1: "Croatia",
-    country2: "Canada",
-  },
-  {
-    matchday: "2022-11-27",
-    matchtime: "22:00",
-    country1: "Spain",
-    country2: "Germany",
-  },
-  {
-    matchday: "2022-11-28",
-    matchtime: "13:00",
-    country1: "Cameroon",
-    country2: "Serbia",
-  },
-  {
-    matchday: "2022-11-28",
-    matchtime: "16:00",
-    country1: "Korea Republic",
-    country2: "Ghana",
-  },
-  {
-    matchday: "2022-11-28",
-    matchtime: "19:00",
-    country1: "Brazil",
-    country2: "Switzerland",
-  },
-  {
-    matchday: "2022-11-28",
-    matchtime: "22:00",
-    country1: "Portugal",
-    country2: "Uruguay",
-  },
-  {
-    matchday: "2022-11-29",
-    matchtime: "18:00",
-    country1: "Ecuador",
-    country2: "Senegal",
-  },
-  {
-    matchday: "2022-11-29",
-    matchtime: "18:00",
-    country1: "Netherlands",
-    country2: "Qatar",
-  },
-  {
-    matchday: "2022-11-29",
-    matchtime: "22:00",
-    country1: "Wales",
-    country2: "England",
-  },
-  {
-    matchday: "2022-11-29",
-    matchtime: "22:00",
-    country1: "Iran",
-    country2: "United States",
-  },
-  {
-    matchday: "2022-11-30",
-    matchtime: "18:00",
-    country1: "Australia",
-    country2: "Denmark",
-  },
-  {
-    matchday: "2022-11-30",
-    matchtime: "18:00",
-    country1: "Tunisia",
-    country2: "France",
-  },
-  {
-    matchday: "2022-11-30",
-    matchtime: "22:00",
-    country1: "Poland",
-    country2: "Argentina",
-  },
-  {
-    matchday: "2022-11-30",
-    matchtime: "22:00",
-    country1: "Saudi Arabia",
-    country2: "Mexico",
-  },
-  {
-    matchday: "2022-12-01",
-    matchtime: "18:00",
-    country1: "Croatia",
-    country2: "Belgium",
-  },
-  {
-    matchday: "2022-12-01",
-    matchtime: "18:00",
-    country1: "Canada",
-    country2: "Morocco",
-  },
-  {
-    matchday: "2022-12-01",
-    matchtime: "22:00",
-    country1: "Japan",
-    country2: "Spain",
-  },
-  {
-    matchday: "2022-12-01",
-    matchtime: "22:00",
-    country1: "Costa Rica",
-    country2: "Germany",
-  },
-  {
-    matchday: "2022-12-02",
-    matchtime: "18:00",
-    country1: "Ghana",
-    country2: "Uruguay",
-  },
-  {
-    matchday: "2022-12-02",
-    matchtime: "18:00",
-    country1: "Korea Republic",
-    country2: "Portugal",
-  },
-  {
-    matchday: "2022-12-02",
-    matchtime: "22:00",
-    country1: "Serbia",
-    country2: "Switzerland",
-  },
-  {
-    matchday: "2022-12-02",
-    matchtime: "22:00",
-    country1: "Cameroon",
-    country2: "Brazil",
-  },
-];
+const matchArr = [ { matchday: "2022-11-20", matchtime: "19:00", country1: "Qatar", country2: "Ecuador", }, { matchday: "2022-11-21", matchtime: "19:00", country1: "Senegal", country2: "Netherlands", }, { matchday: "2022-11-21", matchtime: "16:00", country1: "England", country2: "Iran", }, { matchday: "2022-11-21", matchtime: "22:00", country1: "United States", country2: "Wales", }, { matchday: "2022-11-22", matchtime: "13:00", country1: "Argentina", country2: "Saudi Arabia", }, { matchday: "2022-11-22", matchtime: "16:00", country1: "Denmark", country2: "Tunisia", }, { matchday: "2022-11-22", matchtime: "19:00", country1: "Mexico", country2: "Poland", }, { matchday: "2022-11-22", matchtime: "22:00", country1: "France", country2: "Australia", }, { matchday: "2022-11-23", matchtime: "13:00", country1: "Morocco", country2: "Croatia", }, { matchday: "2022-11-23", matchtime: "16:00", country1: "Germany", country2: "Japan", }, { matchday: "2022-11-23", matchtime: "19:00", country1: "Spain", country2: "Costa Rica", }, { matchday: "2022-11-23", matchtime: "22:00", country1: "Belgium", country2: "Canada", }, { matchday: "2022-11-24", matchtime: "13:00", country1: "Switzerland", country2: "Cameroon", }, { matchday: "2022-11-24", matchtime: "16:00", country1: "Uruguay", country2: "Korea Republic", }, { matchday: "2022-11-24", matchtime: "19:00", country1: "Portugal", country2: "Ghana", }, { matchday: "2022-11-24", matchtime: "22:00", country1: "Brazil", country2: "Serbia", }, { matchday: "2022-11-25", matchtime: "13:00", country1: "Wales", country2: "Iran", }, { matchday: "2022-11-25", matchtime: "16:00", country1: "Qatar", country2: "Senegal", }, { matchday: "2022-11-25", matchtime: "19:00", country1: "Netherlands", country2: "Ecuador", }, { matchday: "2022-11-25", matchtime: "22:00", country1: "England", country2: "United States", }, { matchday: "2022-11-26", matchtime: "13:00", country1: "Tunisia", country2: "Australia", }, { matchday: "2022-11-26", matchtime: "16:00", country1: "Poland", country2: "Saudi Arabia", }, { matchday: "2022-11-26", matchtime: "19:00", country1: "France", country2: "Denmark", }, { matchday: "2022-11-26", matchtime: "22:00", country1: "Argentina", country2: "Mexico", }, { matchday: "2022-11-27", matchtime: "13:00", country1: "Japan", country2: "Costa Rica", }, { matchday: "2022-11-27", matchtime: "16:00", country1: "Belgium", country2: "Morocco", }, { matchday: "2022-11-27", matchtime: "19:00", country1: "Croatia", country2: "Canada", }, { matchday: "2022-11-27", matchtime: "22:00", country1: "Spain", country2: "Germany", }, { matchday: "2022-11-28", matchtime: "13:00", country1: "Cameroon", country2: "Serbia", }, { matchday: "2022-11-28", matchtime: "16:00", country1: "Korea Republic", country2: "Ghana", }, { matchday: "2022-11-28", matchtime: "19:00", country1: "Brazil", country2: "Switzerland", }, { matchday: "2022-11-28", matchtime: "22:00", country1: "Portugal", country2: "Uruguay", }, { matchday: "2022-11-29", matchtime: "18:00", country1: "Ecuador", country2: "Senegal", }, { matchday: "2022-11-29", matchtime: "18:00", country1: "Netherlands", country2: "Qatar", }, { matchday: "2022-11-29", matchtime: "22:00", country1: "Wales", country2: "England", }, { matchday: "2022-11-29", matchtime: "22:00", country1: "Iran", country2: "United States", }, { matchday: "2022-11-30", matchtime: "18:00", country1: "Australia", country2: "Denmark", }, { matchday: "2022-11-30", matchtime: "18:00", country1: "Tunisia", country2: "France", }, { matchday: "2022-11-30", matchtime: "22:00", country1: "Poland", country2: "Argentina", }, { matchday: "2022-11-30", matchtime: "22:00", country1: "Saudi Arabia", country2: "Mexico", }, { matchday: "2022-12-01", matchtime: "18:00", country1: "Croatia", country2: "Belgium", }, { matchday: "2022-12-01", matchtime: "18:00", country1: "Canada", country2: "Morocco", }, { matchday: "2022-12-01", matchtime: "22:00", country1: "Japan", country2: "Spain", }, { matchday: "2022-12-01", matchtime: "22:00", country1: "Costa Rica", country2: "Germany", }, { matchday: "2022-12-02", matchtime: "18:00", country1: "Ghana", country2: "Uruguay", }, { matchday: "2022-12-02", matchtime: "18:00", country1: "Korea Republic", country2: "Portugal", }, { matchday: "2022-12-02", matchtime: "22:00", country1: "Serbia", country2: "Switzerland", }, { matchday: "2022-12-02", matchtime: "22:00", country1: "Cameroon", country2: "Brazil", }, ];
 const Match = mongoose.model("match", matchSchema);
 Match.find({}, function (err, foundList) {
   if (foundList.length == 0) {
@@ -392,13 +104,10 @@ Match.find({}, function (err, foundList) {
 });
 
 const orderSchema = mongoose.Schema({
-  user: userSchema,
-  match: matchSchema,
-  order1: Number,
-  balance1: Number,
-  order2: Number,
-  balance2: Number,
-  balance: Number,
+  userId: String,
+  match: String,
+  order1: {type:Number,default:0},
+  order2: {type:Number,default:0},
 });
 const Order = mongoose.model("order", orderSchema);
 
@@ -455,8 +164,35 @@ app.get("/signout", (req, res) => {
   res.send("Your are logged out ");
 });
 app.get("/dashboard", function (req, res) {
-  const user = "admin";
-  res.render("dashboard");
+  Match.find({}, function (err, matchTable) {
+    if (!err){
+    if (matchTable.length !== 0) {
+      userId=req.session.user
+      Order.find({userId:userId},function(err,userOrder){
+        console.log(userOrder);
+      })
+      res.render("dashboard", {matchTable:matchTable,userId:userId})
+    }}
+  });
 });
+app.post("/order", (req,res)=>{
+  matchid=req.body.matchid
+  userId=req.body.userId
+  
+  try {
+    const order = new Order({
+      userId: req.body.userId,
+      match: req.body.matchid,
+      order1: req.body.order1,
+      order2: req.body.order2,
+    })
+    order.save()
+    
+ } catch (e) {
+    print (e);
+ };
+
+  res.redirect("/dashboard")
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
