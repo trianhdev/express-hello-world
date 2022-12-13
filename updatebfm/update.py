b37625d452d3aab3757325e144168204e67e6cc6
trianhdev <117836328+trianhdev@users.noreply.github.com>
2022-12-13 17:46:09 +0700

    11111111

--- updatebfm/update.py
+++ updatebfm/update.py
@@ -6,33 +6,40 @@ from flatten_dict import unflatten
 from flatten_dict.reducers import make_reducer
 import os
 import glob
-# All files and directories ending with .txt and that don't begin with a dot:
-print(glob.glob("org\*.json")) 
+
+
+# load update excel file
+updatedf = pd.read_excel("update.xlsx", sheet_name='Sheet1',index_col=0)
+
+
+# load json file name
+filepath=glob.glob("org\*.json")
 filelist=[os.path.basename(list_item) for list_item in glob.glob("org\*.json")]
-print((filelist))
-# load excel file
-# df = pd.read_excel("BFMs.xlsx", sheet_name='Sheet1')
-# print(df)
-# load json file
-# with open('de-DE.json', encoding='utf-8') as fh: de = json.load(fh)
+exportfile = pd.DataFrame()
+for file in filepath:
+    filename =os.path.splitext(os.path.basename(file))[0]
+    with open(file, encoding='utf-8') as fh: 
+        data = json.load(fh)
+        # flatten json file
+        dict1 = flatten(data, reducer='dot')
 
+        # export current text for backup
+        exportfile[filename] = pd.DataFrame.from_dict(dict1, orient='index',columns=["content"])["content"]
+        
+        # update dict1
+        if filename in updatedf.columns:
+            for key in updatedf.index:
+                dict1[key] =updatedf.loc[key,filename]
 
-# flatten json file
-# dict1 = flatten(data, reducer='dot')
+        # unflatten dict1
+        dict2 = unflatten(dict1, splitter='dot')
+        # dict2= dict1
+        
+        # export file
+        with open('new\\'+filename+'.json', 'w', encoding='utf8') as outfile:
+            outfile.truncate(0)
+            json.dump(dict2, outfile, ensure_ascii=False)
 
-# update dict
-# for i in df.index:
-    # print({df['Key'][i]})
-    # print(dict1[(df['Key'][i])])
-
-
-# unflatten dict
-# dict2 = unflatten(dict1, splitter='path')
-# dict2= dict1
-
-
-# export file
-# os.mkdir(path, mode)
-# with open('new\zh-CN.json', 'w', encoding='utf8') as outfile:
-#     outfile.truncate(0)
-#     json.dump(dict2, outfile, ensure_ascii=False)
+# export current text for backup
+# exportfile.to_excel("backup.xlsx")
+           
